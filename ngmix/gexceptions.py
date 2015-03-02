54fdee0ee34d8db9fded42ca0491b239eaac927a
Erin Sheldon <erin.sheldon@gmail.com>
2015-03-02 12:27:35 -0500

    removed unused code
    There were commented out old versions of some exceptions.
    Also the same import was listed twice

--- ngmix/gexceptions.py
+++ ngmix/gexceptions.py
@@ -1,6 +1,5 @@
 from ._gmix import GMixRangeError
 from ._gmix import GMixFatalError
-from ._gmix import GMixFatalError
 #from ._gmix import GMixMaxIterEM
 
 class GMixMaxIterEM(Exception):
@@ -11,26 +10,4 @@ class GMixMaxIterEM(Exception):
          self.value = value
     def __str__(self):
         return repr(self.value)
-
-'''
-class GMixRangeError(Exception):
-    """
-    Error for ranges, e.g. ellipticity out of range
-    
-    We usually want to recover gracefully from this error
-    """
-    def __init__(self, value):
-         self.value = value
-    def __str__(self):
-        return repr(self.value)
-
-class GMixFatalError(Exception):
-    """
-    Represents an irrecoverable error
-    """
-    def __init__(self, value):
-         self.value = value
-    def __str__(self):
-        return repr(self.value)
-'''
 
