bcdbd9613fbba86e9ab0e2eed381fb283146cee8
Erin Sheldon <erin.sheldon@gmail.com>
2015-05-21 21:02:49 -0400

    added module to make round observations

---
+++ ngmix/round.py
@@ -0,0 +1,92 @@
+import numpy
+from .observation import Observation,ObsList,MultiBandObsList
+
+def get_round_mb_obs_list(mb_obs_list_in, sim_image=True):
+    """
+    Get roundified version of the MultiBandObsList
+
+    The observations must have
+        - a gmix set, representing the pre-psf object
+        - a psf observation set, with it's own gmix set
+
+    By default the image is simulated to have the round model convolved with
+    the round psf, and noise is added according to the weight map.
+    """
+
+
+    mb_obs_list=MultiBandObsList()
+    for obs_list in mb_obs_list_in:
+        new_obs_list = get_round_obs_list(obs_list,sim_image=sim_image)
+        mb_obs_list.append( new_obs_list )
+
+    return mb_obs_list
+
+
+def get_round_obs_list(obs_list_in, sim_image=True):
+    """
+    Get roundified version of the ObsList
+
+    The observations must have
+        - a gmix set, representing the pre-psf object
+        - a psf observation set, with it's own gmix set
+
+    By default the image is simulated to have the round model convolved with
+    the round psf, and noise is added according to the weight map.
+    """
+
+    obs_list=ObsList()
+    for obs in obs_list_in:
+        new_obs = get_round_obs(obs,sim_image=sim_image)
+        obs_list.append( new_obs )
+
+    return obs_list
+
+def get_round_obs(obs_in, sim_image=True):
+    """
+    Get roundified version of the observation.
+
+    The observation must have
+        - a gmix set, representing the pre-psf object
+        - a psf observation set, with it's own gmix set
+
+    By default the image is simulated to have the round model convolved with
+    the round psf, and noise is added according to the weight map.
+    """
+
+    assert obs_in.has_gmix(),"observation must have a gmix set"
+    assert obs_in.has_psf(),"observation must have a psf set"
+    assert obs_in.psf.has_gmix(),"psf observation must have a gmix set"
+
+    psf_round = obs_in.psf.gmix.make_round()
+    gm0_round = obs_in.gmix.make_round()
+
+    gm_round = gm0_round.convolve(psf_round)
+
+
+    weight = obs_in.weight.copy()
+    if sim_image:
+        noise_image = numpy.random.normal(size=weight.shape)
+        w=numpy.where(weight > 0)
+
+        if w[0].size > 0:
+            noise_image[w] *= 1/sqrt(weight[w])
+
+        imuse = gm_round.make_image(weight.shape,
+                                    jacobian=obs_in.jacobian)
+        imuse += noise_image
+
+        psf_imuse = psf_round.make_image(obs_in.psf.shape,
+                                         jacobian=obs_in.psf.jacobian)
+    else:
+        imuse=obs_in.image.copy()
+        psf_imuse=obs_in.psf.image.copy()
+
+    psf_obs = Observation(psf_imuse,
+                          jacobian=obs.psf.jacobian,
+                          gmix=psf_round)
+    obs=Observation(imuse,
+                    weight=weight,
+                    jacobian=obs.jacobian,
+                    gmix=gm_round,
+                    psf=psf_obs)
+    return obs
