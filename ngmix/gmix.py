adcb62ddfd735988fc877bef5aa3b21cde27d340
Erin Sheldon <erin.sheldon@gmail.com>
2015-08-29 11:05:31 -0400

    print file

--- ngmix/gmix.py
+++ ngmix/gmix.py
@@ -1466,6 +1466,7 @@ class GMixND(object):
         """
         import fitsio
 
+        print("reading mixture from:",fname)
         with fitsio.FITS(fname) as fits:
             weights = fits['weights'].read()
             means = fits['means'].read()
