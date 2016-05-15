ec1eede86eed3f8e65999c2b463cc196e8c196a6
Erin Sheldon <erin.sheldon@gmail.com>
2016-05-15 18:00:44 -0400

    revert em3 guesses
    Go back to the old guesses; the new guesses might have
    looked better for the buggy psfex I was using

--- ngmix/bootstrap.py
+++ ngmix/bootstrap.py
@@ -3059,12 +3059,12 @@ def replace_masked_pixels(mb_obs_list,
 _em2_fguess =array([0.5793612389470884,1.621860687127999])
 _em2_pguess =array([0.596510042804182,0.4034898268889178])
 
-#_em3_pguess = array([0.596510042804182,0.4034898268889178,1.303069003078001e-07])
-#_em3_fguess = array([0.5793612389470884,1.621860687127999,7.019347162356363],dtype='f8')
+_em3_pguess = array([0.596510042804182,0.4034898268889178,1.303069003078001e-07])
+_em3_fguess = array([0.5793612389470884,1.621860687127999,7.019347162356363])
 #_em3_pguess = array([0.7189864,0.2347828,0.04623086])
 #_em3_fguess = array([0.4431912,1.354587,8.274546])
-_em3_pguess = array([0.60,0.36,0.04])
-_em3_fguess = array([0.58,1.62,3.0])
+#_em3_pguess = array([0.60,0.36,0.04])
+#_em3_fguess = array([0.58,1.62,3.0])
 
 def test_boot(model,**keys):
     from .test import make_test_observations
