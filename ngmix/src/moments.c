fdd43d1c37827e9a678f37927b07d866e943594b
Erin Sheldon <erin.sheldon@gmail.com>
2017-09-21 10:48:43 -0400

    unused file

--- ngmix/src/moments.c
+++
@@ -1,9 +0,0 @@
-// Get the weighted moments of the image, using the input gaussian
-// mixture as the weight function.  The moments are *not* normalized
-
-// See the get_weighted_moments method in gmix.py
-
-#include "../_gmix.h"
-#include "gmix.h"
-
-
