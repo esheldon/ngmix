dfc79251f3d2e0944c1c19645faf9dabb69adb93
Erin Sheldon <erin.sheldon@gmail.com>
2017-10-10 15:29:50 -0400

    remove C code

--- ngmix/src/gauleg.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PYGMIX_GAULEG_HEADER_GUARD
-#define _PYGMIX_GAULEG_HEADER_GUARD
-
-int set_gauleg_data(int npoints, const double **xxi, const double **wwi);
-
-#endif
--- ngmix/src/shapes.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PYGMIX_SHAPES_HEADER_GUARD
-#define _PYGMIX_SHAPES_HEADER_GUARD
-
-int g1g2_to_e1e2(double g1, double g2, double *e1, double *e2);
-
-#endif
