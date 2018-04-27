5f6f89786d1b4b958c61677127b68db2e3123e87
Erin Sheldon <erin.sheldon@gmail.com>
2018-04-27 07:29:17 -0700

    add em 4 gauss guess

--- ngmix/bootstrap.py
+++ ngmix/bootstrap.py
@@ -2163,6 +2163,8 @@ class EMRunner(object):
             return self._get_em_guess_2gauss()
         elif self.ngauss==3:
             return self._get_em_guess_3gauss()
+        elif self.ngauss==4:
+            return self._get_em_guess_4gauss()
         else:
             raise ValueError("bad ngauss: %d" % self.ngauss)
 
@@ -2228,6 +2230,44 @@ class EMRunner(object):
 
 
         return GMix(pars=pars)
+
+    def _get_em_guess_4gauss(self):
+
+        sigma2 = self.sigma_guess**2
+
+        pars=array( [_em4_pguess[0]*(1.0+0.1*srandu()),
+                     0.1*srandu(),
+                     0.1*srandu(),
+                     _em4_fguess[0]*sigma2*(1.0 + 0.1*srandu()),
+                     0.01*srandu(),
+                     _em4_fguess[0]*sigma2*(1.0 + 0.1*srandu()),
+
+                     _em4_pguess[1]*(1.0+0.1*srandu()),
+                     0.1*srandu(),
+                     0.1*srandu(),
+                     _em4_fguess[1]*sigma2*(1.0 + 0.1*srandu()),
+                     0.01*srandu(),
+                     _em4_fguess[1]*sigma2*(1.0 + 0.1*srandu()),
+
+                     _em4_pguess[2]*(1.0+0.1*srandu()),
+                     0.1*srandu(),
+                     0.1*srandu(),
+                     _em4_fguess[2]*sigma2*(1.0 + 0.1*srandu()),
+                     0.01*srandu(),
+                     _em4_fguess[2]*sigma2*(1.0 + 0.1*srandu()),
+
+                     _em4_pguess[2]*(1.0+0.1*srandu()),
+                     0.1*srandu(),
+                     0.1*srandu(),
+                     _em4_fguess[2]*sigma2*(1.0 + 0.1*srandu()),
+                     0.01*srandu(),
+                     _em4_fguess[2]*sigma2*(1.0 + 0.1*srandu())]
+
+                  )
+
+
+        return GMix(pars=pars)
+
 
 class PSFRunnerCoellip(object):
     """
@@ -2824,11 +2864,15 @@ def replace_masked_pixels(mb_obs_list,
     return mbo
 
 
-_em2_fguess =array([0.5793612389470884,1.621860687127999])
 _em2_pguess =array([0.596510042804182,0.4034898268889178])
+_em2_fguess =array([0.5793612389470884,1.621860687127999])
 
 _em3_pguess = array([0.596510042804182,0.4034898268889178,1.303069003078001e-07])
 _em3_fguess = array([0.5793612389470884,1.621860687127999,7.019347162356363])
+
+_em4_pguess = array([0.596510042804182,0.4034898268889178,1.303069003078001e-07,1.0e-8])
+_em4_fguess = array([0.5793612389470884,1.621860687127999,7.019347162356363,16.0])
+
 #_em3_pguess = array([0.7189864,0.2347828,0.04623086])
 #_em3_fguess = array([0.4431912,1.354587,8.274546])
 #_em3_pguess = array([0.60,0.36,0.04])
