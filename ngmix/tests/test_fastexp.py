5bd0c7a65abc98f0675026d371d08707ae1da352
Erin Sheldon <erin.sheldon@gmail.com>
2019-10-04 14:54:55 -0400

    use fexp

--- ngmix/tests/test_fastexp.py
+++ ngmix/tests/test_fastexp.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import pytest
 
-from ngmix.fastexp import exp5
+from ngmix.fastexp import fexp
 
 # test values between -15 and 0
 vals = [-7.8864744,  -4.2333561, -11.02660361,  -9.07802778,
@@ -11,14 +11,14 @@ vals = [-7.8864744,  -4.2333561, -11.02660361,  -9.07802778,
 
 @pytest.mark.parametrize('x', vals)
 def test_fastexp_smoke(x):
-    assert np.allclose(np.exp(x), exp5(x), rtol=4.0e-5)
+    assert np.allclose(np.exp(x), fexp(x), rtol=4.0e-5)
 
 
 @pytest.mark.parametrize('x', vals)
 def test_fastexp_timing(x):
     # call a few tims for numba overhead
     for _ in range(2):
-        exp5(x)
+        fexp(x)
 
     t0 = time.time()
     for _ in range(1000):
@@ -27,7 +27,7 @@ def test_fastexp_timing(x):
 
     t0f = time.time()
     for _ in range(1000):
-        exp5(x)
+        fexp(x)
     t0f = time.time() - t0f
 
     # it should be faster
