7d37ace78b985b595ca0e367e992de046a41f369
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-05 13:42:45 -0400

    formatting

--- ngmix/gmix_ndim.py
+++ ngmix/gmix_ndim.py
@@ -8,34 +8,35 @@ loading, as well as very fast likelihood evaluation
 import numpy
 from .gmix_ndim_nb import gmixnd_get_prob
 
+
 class GMixND(object):
     """
     Gaussian mixture in arbitrary dimensions.  A bit awkward
     in dim=1 e.g. becuase assumes means are [ndim,npars]
     """
-    def __init__(self,
-                 weights=None,
-                 means=None,
-                 covars=None,
-                 file=None,
-                 rng=None):
+
+    def __init__(
+        self, weights=None, means=None, covars=None, file=None, rng=None
+    ):
 
         if rng is None:
-            rng=numpy.random.RandomState()
-        self.rng=rng
+            rng = numpy.random.RandomState()
+        self.rng = rng
 
         if file is not None:
             self.load_mixture(file)
         else:
-            if (weights is not None
-                    and means is not None
-                    and covars is not None):
+            if (
+                weights is not None
+                and means is not None
+                and covars is not None
+            ):
                 self.set_mixture(weights, means, covars)
-            elif (weights is not None
-                    or means is not None
-                    or covars is not None):
+            elif (
+                weights is not None or means is not None or covars is not None
+            ):
                 raise RuntimeError(
-                    'send all or none of weights, means, covars'
+                    "send all or none of weights, means, covars"
                 )
 
     def set_mixture(self, weights, means, covars):
@@ -46,24 +47,22 @@ class GMixND(object):
         # copy all to avoid it getting changed under us and to
         # make sure native byte order
 
-        weights = numpy.array(weights, dtype='f8', copy=True)
-        means   = numpy.array(means, dtype='f8', copy=True)
-        covars  = numpy.array(covars, dtype='f8', copy=True)
+        weights = numpy.array(weights, dtype="f8", copy=True)
+        means = numpy.array(means, dtype="f8", copy=True)
+        covars = numpy.array(covars, dtype="f8", copy=True)
 
         if len(means.shape) == 1:
-            means = means.reshape( (means.size, 1) )
+            means = means.reshape((means.size, 1))
         if len(covars.shape) == 1:
-            covars = covars.reshape( (covars.size, 1, 1) )
+            covars = covars.reshape((covars.size, 1, 1))
 
         self.weights = weights
-        self.means=means
-        self.covars=covars
-
-
+        self.means = means
+        self.covars = covars
 
         self.ngauss = self.weights.size
 
-        sh=means.shape
+        sh = means.shape
         if len(sh) == 1:
             raise ValueError("means must be 2-d even for ndim=1")
 
@@ -74,8 +73,9 @@ class GMixND(object):
         self.tmp_lnprob = numpy.zeros(self.ngauss)
         self.xdiff = numpy.zeros(self.ndim)
 
-    def fit(self, data, ngauss, n_iter=5000, min_covar=1.0e-6,
-            doplot=False, **keys):
+    def fit(
+        self, data, ngauss, n_iter=5000, min_covar=1.0e-6, doplot=False, **keys
+    ):
         """
         data is shape
             [npoints, ndim]
@@ -83,17 +83,17 @@ class GMixND(object):
         from sklearn.mixture import GaussianMixture
 
         if len(data.shape) == 1:
-            data = data[:,numpy.newaxis]
+            data = data[:, numpy.newaxis]
 
-        print("ngauss:   ",ngauss)
-        print("n_iter:   ",n_iter)
-        print("min_covar:",min_covar)
+        print("ngauss:   ", ngauss)
+        print("n_iter:   ", n_iter)
+        print("min_covar:", min_covar)
 
-        gmm=GaussianMixture(
+        gmm = GaussianMixture(
             n_components=ngauss,
             max_iter=n_iter,
             reg_covar=min_covar,
-            covariance_type='full',
+            covariance_type="full",
         )
 
         gmm.fit(data)
@@ -101,13 +101,12 @@ class GMixND(object):
         if not gmm.converged_:
             print("DID NOT CONVERGE")
 
-        self._gmm=gmm
+        self._gmm = gmm
         self.set_mixture(gmm.weights_, gmm.means_, gmm.covariances_)
 
         if doplot:
-            plt=self.plot_components(data=data,**keys)
+            plt = self.plot_components(data=data, **keys)
             return plt
-
 
     def save_mixture(self, fname):
         """
@@ -115,11 +114,11 @@ class GMixND(object):
         """
         import fitsio
 
-        print("writing gaussian mixture to :",fname)
-        with fitsio.FITS(fname,'rw',clobber=True) as fits:
-            fits.write(self.weights, extname='weights')
-            fits.write(self.means, extname='means')
-            fits.write(self.covars, extname='covars')
+        print("writing gaussian mixture to :", fname)
+        with fitsio.FITS(fname, "rw", clobber=True) as fits:
+            fits.write(self.weights, extname="weights")
+            fits.write(self.means, extname="means")
+            fits.write(self.covars, extname="covars")
 
     def load_mixture(self, fname):
         """
@@ -127,11 +126,11 @@ class GMixND(object):
         """
         import fitsio
 
-        print("loading gaussian mixture from:",fname)
+        print("loading gaussian mixture from:", fname)
         with fitsio.FITS(fname) as fits:
-            weights = fits['weights'].read()
-            means = fits['means'].read()
-            covars = fits['covars'].read()
+            weights = fits["weights"].read()
+            means = fits["means"].read()
+            covars = fits["covars"].read()
         self.set_mixture(weights, means, covars)
 
     def _get_prob(self, pars, dolog):
@@ -153,11 +152,11 @@ class GMixND(object):
         version with no checking
         """
 
-        n=pars.shape[0]
+        n = pars.shape[0]
         retvals = numpy.zeros(n)
 
         for i in range(n):
-            retvals[i] = self._get_prob(pars[i,:],dolog)
+            retvals[i] = self._get_prob(pars[i, :], dolog)
 
         return retvals
 
@@ -165,16 +164,16 @@ class GMixND(object):
         """
         (x-xmean) icovar (x-xmean)
         """
-        dolog=1
-        pars=numpy.array(pars_in, dtype='f8', ndmin=1, order='C')
+        dolog = 1
+        pars = numpy.array(pars_in, dtype="f8", ndmin=1, order="C")
         return self._get_prob(pars, dolog)
 
     def get_prob_scalar(self, pars_in):
         """
         (x-xmean) icovar (x-xmean)
         """
-        dolog=0
-        pars=numpy.array(pars_in, dtype='f8', ndmin=1, order='C')
+        dolog = 0
+        pars = numpy.array(pars_in, dtype="f8", ndmin=1, order="C")
         return self._get_prob(pars, dolog)
 
     def get_lnprob_array(self, pars):
@@ -182,10 +181,10 @@ class GMixND(object):
         array input
         """
 
-        dolog=1
-        pars=numpy.array(pars, dtype='f8', ndmin=1, order='C')
+        dolog = 1
+        pars = numpy.array(pars, dtype="f8", ndmin=1, order="C")
         if len(pars.shape) == 1:
-            pars = pars[:,numpy.newaxis]
+            pars = pars[:, numpy.newaxis]
 
         return self._get_prob_array(pars, dolog)
 
@@ -194,10 +193,10 @@ class GMixND(object):
         array input
         """
 
-        dolog=0
-        pars=numpy.array(pars, dtype='f8', ndmin=1, order='C')
+        dolog = 0
+        pars = numpy.array(pars, dtype="f8", ndmin=1, order="C")
         if len(pars.shape) == 1:
-            pars = pars[:,numpy.newaxis]
+            pars = pars[:, numpy.newaxis]
 
         return self._get_prob_array(pars, dolog)
 
@@ -205,19 +204,19 @@ class GMixND(object):
         """
         sample from the gaussian mixture
         """
-        if not hasattr(self, '_gmm'):
+        if not hasattr(self, "_gmm"):
             self._set_gmm()
 
         if n is None:
-            is_one=True
-            n=1
+            is_one = True
+            n = 1
         else:
-            is_one=False
+            is_one = False
 
-        samples,labels = self._gmm.sample(n)
+        samples, labels = self._gmm.sample(n)
 
-        if self.ndim==1:
-            samples = samples[:,0]
+        if self.ndim == 1:
+            samples = samples[:, 0]
 
         if is_one:
             samples = samples[0]
@@ -230,37 +229,35 @@ class GMixND(object):
         """
         from sklearn.mixture import GaussianMixture
 
-        gmm=GaussianMixture(
+        gmm = GaussianMixture(
             n_components=ngauss,
             max_iter=10000,
             reg_covar=1.0e-12,
-            covariance_type='full',
+            covariance_type="full",
             random_state=self.rng,
         )
 
         return gmm
 
-
     def _set_gmm(self):
         """
         Make a GMM object for sampling
         """
-        import sklearn.mixture
+        from sklearn.mixture import gaussian_mixture
 
         # these numbers are not used because we set the means, etc by hand
-        ngauss=self.weights.size
+        ngauss = self.weights.size
 
         gmm = self._make_gmm(ngauss)
         gmm.means_ = self.means.copy()
-        #gmm.covars_ = self.covars.copy()
+        # gmm.covars_ = self.covars.copy()
         gmm.covariances_ = self.covars.copy()
         gmm.weights_ = self.weights.copy()
 
-        gmm.precisions_cholesky_ = sklearn.mixture.gaussian_mixture._compute_precision_cholesky(
-            self.covars, 'full',
-        )
+        gmm.precisions_cholesky_ = \
+            gaussian_mixture._compute_precision_cholesky(self.covars, "full")
 
-        self._gmm=gmm
+        self._gmm = gmm
 
     def _calc_icovars_and_norms(self):
         """
@@ -268,26 +265,26 @@ class GMixND(object):
         """
         from numpy import pi
 
-        twopi = 2.0*pi
+        twopi = 2.0 * pi
 
-        #if self.ndim==1:
+        # if self.ndim==1:
         if False:
-            norms = 1.0/numpy.sqrt(twopi*self.covars)
-            icovars = 1.0/self.covars
+            norms = 1.0 / numpy.sqrt(twopi * self.covars)
+            icovars = 1.0 / self.covars
         else:
             norms = numpy.zeros(self.ngauss)
-            icovars = numpy.zeros( (self.ngauss, self.ndim, self.ndim) )
+            icovars = numpy.zeros((self.ngauss, self.ndim, self.ndim))
             for i in range(self.ngauss):
-                cov = self.covars[i,:,:]
-                icov = numpy.linalg.inv( cov )
+                cov = self.covars[i, :, :]
+                icov = numpy.linalg.inv(cov)
 
                 det = numpy.linalg.det(cov)
-                n=1.0/numpy.sqrt( twopi**self.ndim * det )
+                n = 1.0 / numpy.sqrt(twopi ** self.ndim * det)
 
                 norms[i] = n
-                icovars[i,:,:] = icov
+                icovars[i, :, :] = icov
 
         self.norms = norms
-        self.pnorms = norms*self.weights
+        self.pnorms = norms * self.weights
         self.log_pnorms = numpy.log(self.pnorms)
         self.icovars = icovars
