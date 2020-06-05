3a4abd868a31bef06ebd7d47608164ba909d20b0
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-05 10:38:59 -0400

    update ci scripts py36 py37

--- .ci_scripts/install_miniconda.sh
+++ .ci_scripts/install_miniconda.sh
@@ -5,20 +5,22 @@ rm -rf $HOME/miniconda
 
 mkdir -p $HOME/download
 
-if [ "${TOXENV}" = py27 ]; then
-    curl -s https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh -o $HOME/download/miniconda.sh;
-fi
-if [ "${TOXENV}" = py36 ]; then
-    curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o $HOME/download/miniconda.sh;
-fi
+curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o $HOME/download/miniconda.sh;
 
 bash $HOME/download/miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
+
 cp .ci_scripts/condarc $HOME/miniconda/.condarc
 conda update -q conda
 conda info -a
-if [ "${TOXENV}" = py27 ]; then pyver=2.7; fi
-if [ "${TOXENV}" = py36 ]; then pyver=3.6; fi
+
+if [ "${TOXENV}" = py36 ]; then
+    pyver=3.6
+fi
+if [ "${TOXENV}" = py37 ]; then
+    pyver=3.7
+fi
+
 conda create -q -n test-env python=${pyver} pip setuptools \
     numpy numba flake8 pyyaml scipy pytest galsim \
     scikit-learn statsmodels emcee
