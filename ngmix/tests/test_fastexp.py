357936563f7301becd989dcea661c12e9fd3cdda
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-05 13:18:11 -0400

    formatting

--- ngmix/tests/test_fastexp.py
+++ ngmix/tests/test_fastexp.py
@@ -5,8 +5,8 @@ import pytest
 from ngmix.fastexp import fexp
 
 # test values between -15 and 0
-vals = [-7.8864744,  -4.2333561, -11.02660361,  -9.07802778,
-        -12.01531878,  -8.4256256,  -8.70588303]
+vals = [-7.8864744, -4.2333561, -11.02660361, -9.07802778,
+        -12.01531878, -8.4256256, -8.70588303]
 
 
 @pytest.mark.parametrize('x', vals)
