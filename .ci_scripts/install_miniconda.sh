ce3f93995f2a10b81bdf98b9b07f96c5d7ac2c61
Erin Sheldon <erin.sheldon@gmail.com>
2020-06-05 11:04:00 -0400

    add back py27 without flake8

--- .ci_scripts/install_miniconda.sh
+++ .ci_scripts/install_miniconda.sh
@@ -5,7 +5,11 @@ rm -rf $HOME/miniconda
 
 mkdir -p $HOME/download
 
-curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o $HOME/download/miniconda.sh;
+if [[ ${TOXENV} == "py27" ]]; then
+    curl -s https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh -o $HOME/download/miniconda.sh;
+else
+    curl -s https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o $HOME/download/miniconda.sh;
+fi
 
 bash $HOME/download/miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
@@ -14,6 +18,9 @@ cp .ci_scripts/condarc $HOME/miniconda/.condarc
 conda update -q conda
 conda info -a
 
+if [ "${TOXENV}" = py27 ]; then
+    pyver=2.7
+fi
 if [ "${TOXENV}" = py36 ]; then
     pyver=3.6
 fi
