75a1b7afb31f8e51ae56bf1eb0e559e508ad5dd8
Matthew R. Becker <becker.mr@gmail.com>
2020-09-21 17:23:53 -0500

    TST added more tests for kobs

--- ngmix/tests/test_kobs.py
+++ ngmix/tests/test_kobs.py
@@ -5,6 +5,66 @@ import galsim
 from ngmix.observation import KObservation, KObsList, KMultiBandObsList, get_kmb_obs
 
 
+def test_kobslist():
+    rng = np.random.RandomState(seed=11)
+    obs1 = KObservation(kimage=galsim.ImageCD(rng.normal(size=(11, 13)), scale=0.3))
+    obs2 = KObservation(kimage=galsim.ImageCD(rng.normal(size=(11, 13)), scale=0.3))
+
+    kobslist = KObsList()
+    kobslist.append(obs1)
+    assert np.all(kobslist[0].kimage.array == obs1.kimage.array)
+    kobslist.append(obs2)
+    assert np.all(kobslist[0].kimage.array == obs1.kimage.array)
+    assert np.all(kobslist[1].kimage.array == obs2.kimage.array)
+    kobslist[1] = obs1
+    assert np.all(kobslist[1].kimage.array == obs1.kimage.array)
+
+    with pytest.raises(AssertionError):
+        kobslist.append(None)
+
+    with pytest.raises(AssertionError):
+        kobslist[0] = None
+
+
+def test_kmbobslist():
+    rng = np.random.RandomState(seed=11)
+    obs1 = KObservation(kimage=galsim.ImageCD(rng.normal(size=(11, 13)), scale=0.3))
+    obs2 = KObservation(kimage=galsim.ImageCD(rng.normal(size=(11, 13)), scale=0.3))
+    obs3 = KObservation(kimage=galsim.ImageCD(rng.normal(size=(11, 13)), scale=0.3))
+    kobslist1 = KObsList()
+    kobslist1.append(obs1)
+    kobslist1.append(obs2)
+    kobslist2 = KObsList()
+    kobslist2.append(obs3)
+
+    kmbobs = KMultiBandObsList()
+    kmbobs.append(kobslist1)
+    assert all(
+        np.all(obs.kimage.array == _obs.kimage.array)
+        for obs, _obs in zip(kmbobs[0], kobslist1)
+    )
+    kmbobs.append(kobslist2)
+    assert all(
+        np.all(obs.kimage.array == _obs.kimage.array)
+        for obs, _obs in zip(kmbobs[0], kobslist1)
+    )
+    assert all(
+        np.all(obs.kimage.array == _obs.kimage.array)
+        for obs, _obs in zip(kmbobs[1], kobslist2)
+    )
+    kmbobs[1] = kobslist1
+    assert all(
+        np.all(obs.kimage.array == _obs.kimage.array)
+        for obs, _obs in zip(kmbobs[1], kobslist1)
+    )
+
+    with pytest.raises(AssertionError):
+        kmbobs.append(None)
+
+    with pytest.raises(AssertionError):
+        kmbobs[0] = None
+
+
 def test_get_kmb_obs():
     rng = np.random.RandomState(seed=11)
     obs = KObservation(kimage=galsim.ImageCD(rng.normal(size=(11, 13)), scale=0.3))
