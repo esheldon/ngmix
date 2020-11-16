1b4003f754a3cff8cd7504c82815649f0cd5ed50
Matthew R. Becker <becker.mr@gmail.com>
2020-11-16 15:38:39 -0600

    Revert "REF more module level changes to code"
    This reverts commit 81657807b51b036514177af02a7f6a6012101e74.

--- ngmix/priors/__init__.py
+++ ngmix/priors/__init__.py
@@ -1,5 +1,3 @@
 # flake8: noqa
 from ._priors import *
 from ._random import make_rng, srandu
-from ._constants import LOWVAL, BIGVAL
-from ._base import PriorBase, GPriorBase
--- ngmix/priors/_constants.py
+++
@@ -1,4 +0,0 @@
-import numpy
-
-LOWVAL = -numpy.inf
-BIGVAL = 9999.0e47
