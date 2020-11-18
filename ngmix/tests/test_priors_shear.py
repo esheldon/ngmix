a3226c9c28428fb102493fec7fa169c87b55debc
Matthew R. Becker <becker.mr@gmail.com>
2020-11-18 15:16:52 -0600

    TST more tests for BA

--- ngmix/tests/test_priors_shear.py
+++ ngmix/tests/test_priors_shear.py
@@ -43,17 +43,91 @@ def test_priors_gpriorba():
     assert pr.sigma == 0.5
     assert pr.A == 1.0
 
-    # make sure histogram of 1d samples matches prob dist we expect
-    g_samps = pr.sample1d(200000)
-    h, be = np.histogram(g_samps, bins=np.linspace(0, 1, 100))
-    h = h / np.sum(h)
-    bc = (be[1:] + be[:-1])/2.0
-    g_probs = pr.get_prob_array1d(bc)
-    g_probs = g_probs / np.sum(g_probs)
-    assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
-    for i in range(len(bc)):
-        g_probs[i] = pr.get_prob_scalar1d(bc[i])
-    g_probs = g_probs / np.sum(g_probs)
-    assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+    g1, g2 = pr.sample2d()
+    assert isinstance(g1, float)
+    assert isinstance(g2, float)
 
-    # do the same test but in 2d
+    # make sure histogram of samples matches prob dist we expect
+    for i in range(3):
+        if i == 0:
+            g1, g2 = None, None
+            g_samps = pr.sample1d(200000)
+        elif i == 1:
+            g1, g2 = pr.sample2d_brute(200000)
+            g_samps = np.sqrt(g1**2 + g2**2)
+        else:
+            g1, g2 = pr.sample2d(200000)
+            g_samps = np.sqrt(g1**2 + g2**2)
+
+        h, be = np.histogram(g_samps, bins=np.linspace(0, 1, 100))
+        h = h / np.sum(h)
+        bc = (be[1:] + be[:-1])/2.0
+
+        g_probs = pr.get_prob_array1d(bc)
+        g_probs = g_probs / np.sum(g_probs)
+        assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+
+        for i in range(len(bc)):
+            g_probs[i] = pr.get_prob_scalar1d(bc[i])
+        g_probs = g_probs / np.sum(g_probs)
+        assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+
+        # test 2d via using 1d samples and factor of g
+        g_probs = pr.get_prob_array2d(bc, np.zeros_like(bc)) * bc
+        g_probs = g_probs / np.sum(g_probs)
+        assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+
+        for i in range(len(bc)):
+            g_probs[i] = pr.get_prob_scalar2d(bc[i], 0.0) * bc[i]
+        g_probs = g_probs / np.sum(g_probs)
+        assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+
+        g_probs = np.exp(pr.get_lnprob_array2d(bc, np.zeros_like(bc))) * bc
+        g_probs = g_probs / np.sum(g_probs)
+        assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+
+        for i in range(len(bc)):
+            g_probs[i] = np.exp(pr.get_lnprob_scalar2d(bc[i], 0.0)) * bc[i]
+        g_probs = g_probs / np.sum(g_probs)
+        assert np.allclose(h, g_probs, atol=1e-3, rtol=0), np.max(np.abs(h-g_probs))
+
+        # test 1d in each comp
+        for gc in [g1, g2]:
+            if gc is not None:
+                h, be = np.histogram(gc, bins=np.linspace(0, 1, 100))
+                h = h / np.sum(h)
+                bc = (be[1:] + be[:-1])/2.0
+
+                g_probs = pr.get_prob_array2d(bc, np.zeros_like(bc))
+                g_probs = g_probs / np.sum(g_probs)
+                assert np.allclose(h, g_probs, atol=3e-3, rtol=0), (
+                    np.max(np.abs(h-g_probs))
+                )
+
+                for i in range(len(bc)):
+                    g_probs[i] = pr.get_prob_scalar2d(bc[i], 0.0)
+                g_probs = g_probs / np.sum(g_probs)
+                assert np.allclose(h, g_probs, atol=3e-3, rtol=0), (
+                    np.max(np.abs(h-g_probs))
+                )
+
+                g_probs = np.exp(pr.get_lnprob_array2d(bc, np.zeros_like(bc)))
+                g_probs = g_probs / np.sum(g_probs)
+                assert np.allclose(h, g_probs, atol=3e-3, rtol=0), (
+                    np.max(np.abs(h-g_probs))
+                )
+
+                for i in range(len(bc)):
+                    g_probs[i] = np.exp(pr.get_lnprob_scalar2d(bc[i], 0.0))
+                g_probs = g_probs / np.sum(g_probs)
+                assert np.allclose(h, g_probs, atol=3e-3, rtol=0), (
+                    np.max(np.abs(h-g_probs))
+                )
+
+    # make sure recomputing the max value kind of works
+    mvloc, mv = pr.maxval1d_loc, pr.maxval1d
+    pr.set_maxval1d_scipy()
+    assert np.allclose(mvloc, pr.maxval1d_loc, atol=1e-2, rtol=0)
+    assert np.allclose(mv, pr.maxval1d, atol=1e-2, rtol=0)
+
+    # finally try and fit samples of g1, g2 to get back the prior we put in
