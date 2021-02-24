c12aa82a8da1e527f492d673c12e59d4819ab689
Erin Sheldon <erin.sheldon@gmail.com>
2021-02-24 13:26:02 -0500

    add test of prior based guessers

--- ngmix/tests/test_guessers.py
+++ ngmix/tests/test_guessers.py
@@ -60,8 +60,12 @@ def test_noprior_guessers_smoke(guesser_type, nband):
     assert guess.size == npars
 
 
+@pytest.mark.parametrize(
+    'guesser_type',
+    ['TFluxAndPrior', 'TPSFFluxAndPrior', 'BD', 'BDF', 'Prior'],
+)
 @pytest.mark.parametrize('nband', [None, 1, 2])
-def test_T_flux_prior_guesser_smoke(nband):
+def test_prior_guessers_smoke(guesser_type, nband):
     nband_use = nband if nband is not None else 1
 
     rng = np.random.RandomState(19487)
@@ -72,18 +76,54 @@ def test_T_flux_prior_guesser_smoke(nband):
         nband=nband,
     )
 
-    prior = ngmix.joint_prior.PriorSimpleSep(
-        cen_prior=ngmix.priors.CenPrior(0.0, 0.0, 0.1, 0.1, rng=rng),
-        g_prior=ngmix.priors.GPriorBA(0.3, rng=rng),
-        T_prior=ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng),
-        F_prior=[ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng)]*nband_use,
-    )
     T_center = 0.001
     flux_center = [1.0]*nband_use
+    if 'Prior' in guesser_type:
+        prior = ngmix.joint_prior.PriorSimpleSep(
+            cen_prior=ngmix.priors.CenPrior(0.0, 0.0, 0.1, 0.1, rng=rng),
+            g_prior=ngmix.priors.GPriorBA(0.3, rng=rng),
+            T_prior=ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng),
+            F_prior=[ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng)]*nband_use,
+        )
 
-    guesser = guessers.TFluxAndPriorGuesser(
-        rng=rng, T=T_center, flux=flux_center, prior=prior,
-    )
+        if 'PSF' in guesser_type:
+            guesser = guessers.TPSFFluxAndPriorGuesser(
+                rng=rng, T=T_center, prior=prior,
+            )
+        elif 'Flux' in guesser_type:
+            guesser = guessers.TFluxAndPriorGuesser(
+                rng=rng, T=T_center, flux=flux_center, prior=prior,
+            )
+        else:
+            guesser = guessers.PriorGuesser(prior=prior)
+        npars = 5 + nband_use
+    elif guesser_type == 'BD':
+        prior = ngmix.joint_prior.PriorBDSep(
+            cen_prior=ngmix.priors.CenPrior(0.0, 0.0, 0.1, 0.1, rng=rng),
+            g_prior=ngmix.priors.GPriorBA(0.3, rng=rng),
+            logTratio_prior=ngmix.priors.Normal(1.0, 0.5, rng=rng),
+            fracdev_prior=ngmix.priors.Normal(0.5, 0.1, rng=rng),
+            T_prior=ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng),
+            F_prior=[ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng)]*nband_use,
+        )
+        guesser = guessers.BDGuesser(
+            T=T_center, flux=flux_center, prior=prior,
+        )
+        npars = 7 + nband_use
+    elif guesser_type == 'BDF':
+        prior = ngmix.joint_prior.PriorBDFSep(
+            cen_prior=ngmix.priors.CenPrior(0.0, 0.0, 0.1, 0.1, rng=rng),
+            g_prior=ngmix.priors.GPriorBA(0.3, rng=rng),
+            fracdev_prior=ngmix.priors.Normal(0.5, 0.1, rng=rng),
+            T_prior=ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng),
+            F_prior=[ngmix.priors.FlatPrior(-1.0, 1.e5, rng=rng)]*nband_use,
+        )
+        guesser = guessers.BDFGuesser(
+            T=T_center, flux=flux_center, prior=prior,
+        )
+        npars = 6 + nband_use
+    else:
+        raise ValueError('bad guesser %s' % guesser_type)
 
     if nband is None:
         obs_use = data['obslist']
@@ -91,5 +131,4 @@ def test_T_flux_prior_guesser_smoke(nband):
         obs_use = data['mbobs']
 
     guess = guesser(obs=obs_use)
-    nband_use = nband if nband is not None else 1
-    assert guess.size == 5 + nband_use
+    assert guess.size == npars
