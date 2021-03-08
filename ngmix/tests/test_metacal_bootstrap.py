d857157c79e3214cc67cdc5a091db9ceb694c7bb
Erin Sheldon <erin.sheldon@gmail.com>
2021-03-08 10:30:29 -0500

    test actual mean response value for gaussmom
    use a low noise image, and test against the response from ngmix 1.3.8

--- ngmix/tests/test_metacal_bootstrap.py
+++ ngmix/tests/test_metacal_bootstrap.py
@@ -10,6 +10,7 @@ from ngmix.gaussmom import GaussMom
 from ngmix.metacal_bootstrap import metacal_bootstrap, MetacalBootstrapper
 from ._sims import get_model_obs
 from ._priors import get_prior
+from ._galsim_sims import _get_obs
 
 FRAC_TOL = 5.0e-4
 
@@ -130,3 +131,47 @@ def test_metacal_bootstrap_gaussmom_smoke(noise, use_bootstrapper):
     for key in ['noshear', '1p', '1m', '2p', '2m']:
         assert key in resdict
         assert resdict[key]['flags'] == 0
+
+
+def test_metacal_bootstrap_gaussmom_response():
+    """
+    test a metacal bootstrapper with gaussian moments
+    """
+
+    rng = np.random.RandomState(2830)
+    ntrial = 50
+    mcal_kws = {'psf': 'gauss', 'types': ['1p', '1m']}
+
+    fwhm = 1.2
+    fitter = GaussMom(fwhm=fwhm)
+    psf_fitter = GaussMom(fwhm=fwhm)
+
+    psf_runner = PSFRunner(fitter=psf_fitter)
+    runner = Runner(fitter=fitter)
+
+    boot = MetacalBootstrapper(
+        runner=runner, psf_runner=psf_runner,
+        rng=rng,
+        **mcal_kws,
+    )
+
+    Rvals = np.zeros(ntrial)
+    for i in range(ntrial):
+        obs = _get_obs(
+            rng=rng,
+            set_noise_image=False,
+        )
+
+        boot.go(obs)
+        resdict = boot.get_result()
+
+        res1p = resdict['1p']
+        res1m = resdict['1m']
+
+        Rvals[i] = (res1p['e'][0] - res1m['e'][0])/0.02
+
+    Rmean = Rvals.mean()
+    # this response value comes from ngmix 1.3.8, and had an error
+    # of 2.7e-5 on it.  Allow for differences in rng/arch etc. by
+    # taking 3*2.7e-5 = 1.3e-4
+    assert abs(Rmean - 0.276829) < 1.3e-4
