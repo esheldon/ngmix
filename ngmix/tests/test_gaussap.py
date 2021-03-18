985ac48b073745c1c5735aaa3e60dda871cfc827
Erin Sheldon <erin.sheldon@gmail.com>
2021-03-18 11:52:09 -0400

    gaussap tests

--- ngmix/tests/test_gaussap.py
+++ ngmix/tests/test_gaussap.py
@@ -30,23 +30,29 @@ def test_gaussap_simple_smoke(model, weight_fwhm):
 
         pars[i, :] = this_pars
 
+    mask = np.ones(nobj, dtype=bool)
+    ibad = 5
+    mask[ibad] = False
     gap_fluxes, flags = ngmix.gaussap.get_gaussap_flux(
         pars,
         model,
         weight_fwhm,
+        mask=mask,
     )
 
-    assert np.all(flags == 0)
+    w, = np.where(flags[:, 0] == 0)
+    assert flags[ibad, 0] != 0
+    assert w.size == nobj-1
 
     bstart = 5
     assert gap_fluxes.shape == pars[:, bstart:bstart+nband].shape
 
     if weight_fwhm > 100:
         # for large weight functions we should recover the original flux
-        assert np.all(np.abs(pars[:, bstart:bstart+nband] - gap_fluxes) < 0.01)
+        assert np.all(np.abs(pars[w, bstart:bstart+nband] - gap_fluxes[w]) < 0.01)
     else:
         # for smaller weight functions we just verify the flux is smaller
-        assert np.all(gap_fluxes < pars[:, bstart:bstart+nband])
+        assert np.all(gap_fluxes[w] < pars[w, bstart:bstart+nband])
 
 
 def test_gaussap_cm_smoke():
@@ -140,3 +146,29 @@ def test_gaussap_bdf_smoke():
     else:
         # for smaller weight functions we just verify the flux is smaller
         assert np.all(gap_fluxes < pars[:, bstart:bstart+nband])
+
+
+@pytest.mark.parametrize('nobj', [1, 10])
+def test_gaussap_corner_cases(nobj):
+    rng = np.random.RandomState(seed=31415)
+
+    model = 'exp'
+    weight_fwhm = 2.0
+
+    pars = np.zeros((nobj, 6))
+
+    pars[:, 0] = rng.uniform(low=-0.1, high=0.1, size=nobj)  # cen1
+    pars[:, 1] = rng.uniform(low=-0.1, high=0.1, size=nobj)  # cen2
+    pars[:, 2] = rng.uniform(low=-0.1, high=0.1, size=nobj)  # g1
+    pars[:, 3] = rng.uniform(low=-0.1, high=0.1, size=nobj)  # g2
+    pars[:, 4] = rng.uniform(low=0.2, high=1.0, size=nobj)  # T
+    pars[:, 5] = rng.uniform(low=100, high=200, size=nobj)  # flux
+
+    ibad = 0
+    pars[ibad, 2] = 999
+
+    if nobj == 1:
+        pars = pars[0, :]
+
+    gap_fluxes, flags = ngmix.gaussap.get_gaussap_flux(pars, model, weight_fwhm)
+    assert flags[ibad] != 0
