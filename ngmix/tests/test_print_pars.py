ca87dccdd612f3d6006f948d763b543da50700f8
Erin Sheldon <erin.sheldon@gmail.com>
2021-03-19 09:20:04 -0400

    added tester for util.print_pars

---
+++ ngmix/tests/test_print_pars.py
@@ -0,0 +1,13 @@
+import numpy as np
+from ..util import print_pars
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def test_print_pars():
+
+    pars = [0.2, -0.1, 0.2, 0.3, 15.0, 123.1]
+    print_pars(pars)
+    print_pars(pars, front='pars: ')
+    print_pars(pars, logger=logger)
