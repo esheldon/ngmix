f7e9a99190fe02400969471548a9672cab449e8f
Matthew R. Becker <becker.mr@gmail.com>
2021-04-26 09:09:16 -0500

    REF better tests

--- ngmix/tests/test_prepsfmom.py
+++ ngmix/tests/test_prepsfmom.py
@@ -114,15 +114,27 @@ def test_prepsfmom_smoke(g1_true, g2_true, wcs_g1, wcs_g2, weight_fac):
     assert np.abs(mean_flux - flux_true) < 5e-4, (np.mean(farr), np.std(farr))
 
 
+def _report_info(s, arr, mn, err):
+    print(
+        "%s:" % s,
+        np.mean(arr), mn, np.mean(arr)/mn - 1,
+        np.std(arr), err, np.std(arr)/err - 1,
+    )
+
+
+@pytest.mark.parametrize('pixel_scale', [0.125, 0.5])
 @pytest.mark.parametrize('image_size', [107, 112])
 @pytest.mark.parametrize('pad_factor', [1, 3.1, 1.3234])
-def test_prepsfmom_error(pad_factor, image_size):
+def test_prepsfmom_error(pad_factor, image_size, pixel_scale):
     rng = np.random.RandomState(seed=100)
 
     fwhm = 0.9
     cen = (image_size - 1)/2
     gs_wcs = galsim.ShearWCS(
-        0.125, galsim.Shear(g1=-0.1, g2=0.23)).jacobian()
+        pixel_scale, galsim.Shear(g1=-0.1, g2=0.23)).jacobian()
+    scale = np.sqrt(gs_wcs.pixelArea())
+    shift = rng.uniform(low=-scale/2, high=scale/2, size=2) * 0
+    xy = gs_wcs.toImage(galsim.PositionD(shift))
 
     obj = galsim.Gaussian(
         fwhm=fwhm
@@ -130,14 +142,15 @@ def test_prepsfmom_error(pad_factor, image_size):
         g1=-0.1, g2=0.1
     ).withFlux(
         400)
-    im = obj.drawImage(
+    im = obj.shift(
+        dx=shift[0], dy=shift[1]
+    ).drawImage(
         nx=image_size,
         ny=image_size,
         wcs=gs_wcs,
         method='no_pixel').array
     noise = np.sqrt(np.sum(im**2)) / 1e3
     wgt = np.ones_like(im) / noise**2
-    scale = np.sqrt(gs_wcs.pixelArea())
 
     g1arr = []
     g2arr = []
@@ -148,7 +161,7 @@ def test_prepsfmom_error(pad_factor, image_size):
 
     # get true flux
     jac = Jacobian(
-        y=cen, x=cen,
+        y=cen + xy.y, x=cen + xy.x,
         dudx=gs_wcs.dudx, dudy=gs_wcs.dudy,
         dvdx=gs_wcs.dvdx, dvdy=gs_wcs.dvdy)
     obs = Observation(
@@ -162,23 +175,6 @@ def test_prepsfmom_error(pad_factor, image_size):
     g2_true = res["e"][1]
 
     for _ in range(1000):
-        shift = rng.uniform(low=-scale/2, high=scale/2, size=2)
-        xy = gs_wcs.toImage(galsim.PositionD(shift))
-
-        im = obj.shift(
-            dx=shift[0], dy=shift[1]
-        ).drawImage(
-            nx=image_size,
-            ny=image_size,
-            wcs=gs_wcs,
-            method='no_pixel',
-            dtype=np.float64).array
-
-        jac = Jacobian(
-            y=cen + xy.y, x=cen + xy.x,
-            dudx=gs_wcs.dudx, dudy=gs_wcs.dudy,
-            dvdx=gs_wcs.dvdx, dvdy=gs_wcs.dvdy)
-
         _im = im + (rng.normal(size=im.shape) * noise)
         obs = Observation(
             image=_im,
@@ -195,6 +191,12 @@ def test_prepsfmom_error(pad_factor, image_size):
             Tarr.append(res['T'])
             farr.append(res['flux'])
 
+    print("\n")
+    _report_info("flux", farr, flux_true, res["flux_err"])
+    _report_info("T", Tarr, T_true, res["T_err"])
+    _report_info("g1", g1arr, g1_true, res["e_err"][0])
+    _report_info("g2", g2arr, g2_true, res["e_err"][1])
+
     assert np.allclose(np.mean(farr), flux_true, atol=0, rtol=1e-3)
     assert np.allclose(np.mean(Tarr), T_true, atol=0, rtol=1e-3)
     assert np.allclose(np.mean(g1arr), g1_true, atol=0, rtol=1e-2)
@@ -207,31 +209,29 @@ def test_prepsfmom_error(pad_factor, image_size):
     assert np.allclose(np.std(g2arr), res["e_err"][1], atol=0, rtol=etol*3)
 
 
-def _report_info(s, arr, mn, err):
-    print(
-        "%s:" % s,
-        np.mean(arr), mn, np.mean(arr)/mn - 1,
-        np.std(arr), err, np.std(arr)/err - 1,
-    )
-
-
 @pytest.mark.parametrize('direct_deconv', [False, True])
 @pytest.mark.parametrize('fwhm,psf_fwhm', [(0.6, 0.9), (1.5, 0.9)])
 @pytest.mark.parametrize('image_size', [107, 110])
-@pytest.mark.parametrize('pad_factor', [4, 2.5, 1, 1.2])
+@pytest.mark.parametrize('pad_factor', [2, 1, 1.5])
 def test_prepsfmom_psf(pad_factor, image_size, fwhm, psf_fwhm, direct_deconv):
     rng = np.random.RandomState(seed=100)
 
     cen = (image_size - 1)/2
     gs_wcs = galsim.ShearWCS(
         0.125, galsim.Shear(g1=-0.1, g2=0.06)).jacobian()
+    scale = np.sqrt(gs_wcs.pixelArea())
+    shift = rng.uniform(low=-scale/2, high=scale/2, size=2)
+    xy = gs_wcs.toImage(galsim.PositionD(shift))
 
     gal = galsim.Gaussian(
         fwhm=fwhm
     ).shear(
         g1=-0.1, g2=0.1
     ).withFlux(
-        400)
+        400
+    ).shift(
+        dx=shift[0], dy=shift[1]
+    )
     psf = galsim.Gaussian(
         fwhm=psf_fwhm
     ).shear(
@@ -243,9 +243,10 @@ def test_prepsfmom_psf(pad_factor, image_size, fwhm, psf_fwhm, direct_deconv):
         wcs=gs_wcs).array
     noise = np.sqrt(np.sum(im**2)) / 1e3
     wgt = np.ones_like(im) / noise**2
-    scale = np.sqrt(gs_wcs.pixelArea())
 
-    psf_im = psf.drawImage(
+    psf_im = psf.shift(
+        dx=shift[0], dy=shift[1]
+    ).drawImage(
         nx=image_size,
         ny=image_size,
         wcs=gs_wcs).array
@@ -263,17 +264,17 @@ def test_prepsfmom_psf(pad_factor, image_size, fwhm, psf_fwhm, direct_deconv):
     )
 
     # get true flux
-    im = gal.drawImage(
+    im_true = gal.drawImage(
         nx=image_size,
         ny=image_size,
         wcs=gs_wcs,
         method='no_pixel').array
     jac = Jacobian(
-        y=cen, x=cen,
+        y=cen + xy.y, x=cen + xy.x,
         dudx=gs_wcs.dudx, dudy=gs_wcs.dudy,
         dvdx=gs_wcs.dvdx, dvdy=gs_wcs.dvdy)
     obs = Observation(
-        image=im,
+        image=im_true,
         jacobian=jac,
     )
     res = fitter.go(obs=obs)
@@ -283,34 +284,11 @@ def test_prepsfmom_psf(pad_factor, image_size, fwhm, psf_fwhm, direct_deconv):
     g2_true = res["e"][1]
 
     for _ in range(100):
-        shift = rng.uniform(low=-scale/2, high=scale/2, size=2)
-        xy = gs_wcs.toImage(galsim.PositionD(shift))
-
-        im = galsim.Convolve([gal, psf]).shift(
-            dx=shift[0], dy=shift[1]
-        ).drawImage(
-            nx=image_size,
-            ny=image_size,
-            wcs=gs_wcs,
-            dtype=np.float64).array
-
-        psf_im = psf.shift(
-            dx=shift[0]*0, dy=shift[1]*0
-        ).drawImage(
-            nx=image_size,
-            ny=image_size,
-            wcs=gs_wcs).array
-
-        _jac = Jacobian(
-            y=cen + xy.y, x=cen + xy.x,
-            dudx=gs_wcs.dudx, dudy=gs_wcs.dudy,
-            dvdx=gs_wcs.dvdx, dvdy=gs_wcs.dvdy)
-
         _im = im + (rng.normal(size=im.shape) * noise)
         obs = Observation(
             image=_im,
             weight=wgt,
-            jacobian=_jac,
+            jacobian=jac,
             psf=Observation(image=psf_im, jacobian=jac),
         )
 
@@ -339,7 +317,7 @@ def test_prepsfmom_psf(pad_factor, image_size, fwhm, psf_fwhm, direct_deconv):
         etol = 2e-1
         gfac = 1
     else:
-        etol = 1e-2
+        etol = 2e-2
         gfac = 10
     assert np.allclose(np.mean(farr), flux_true, atol=0, rtol=etol)
     assert np.allclose(np.mean(Tarr), T_true, atol=0, rtol=etol)
@@ -348,7 +326,7 @@ def test_prepsfmom_psf(pad_factor, image_size, fwhm, psf_fwhm, direct_deconv):
 
     if not direct_deconv:
         # errors are only currently right for fourier version
-        etol = 0.15
+        etol = 0.2
         assert np.allclose(np.std(farr), res["flux_err"], atol=0, rtol=etol)
         assert np.allclose(np.std(Tarr), res["T_err"], atol=0, rtol=etol)
         assert np.allclose(np.std(g1arr), res["e_err"][0], atol=0, rtol=etol*2)
