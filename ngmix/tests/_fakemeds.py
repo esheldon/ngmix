32170d4923fbe7265cfb1f3cf8cb0f2b9f17ae42
Matthew R. Becker <becker.mr@gmail.com>
2021-04-28 10:11:15 -0500

    BUG wrong params to uniform generator

--- ngmix/tests/_fakemeds.py
+++ ngmix/tests/_fakemeds.py
@@ -104,7 +104,7 @@ def write_cutouts(
                 elif cutout_type == 'noise':
                     imdata = rng.normal(scale=noise, size=imdata.shape)
                 elif cutout_type == 'mfrac':
-                    imdata = rng.uniform(scale=noise, size=imdata.shape)
+                    imdata = rng.uniform(size=imdata.shape)
                 elif cutout_type == 'psf':
                     row, col = [(box_size - 1)/2]*2
                     imdata = make_model_image(
