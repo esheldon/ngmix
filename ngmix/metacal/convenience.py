5afc23edb5e7278187aca261d46c764d88610084
Erin Sheldon <erin.sheldon@gmail.com>
2021-11-02 13:59:51 -0400

    more clarification

--- ngmix/metacal/convenience.py
+++ ngmix/metacal/convenience.py
@@ -53,9 +53,8 @@ def get_all_metacal(
         A random number generator; this is required if fixnoise is True and
         use_noise_image is False, or if psf is set to 'fitgauss'.
 
-        If the keyword psf= is set to 'gauss' or is a galsim object, it will be
-        used to to add a small amount of noise to the rendered image of the
-        psf, but is not required.
+        For psf is set to 'gauss', 'fitgauss' or a galsim object, it will be
+        used to to add a small amount of noise to the rendered image of the psf
     use_noise_image: bool, optional
         If set to True, use the .noise attribute of the observation
         for fixing the noise when fixnoise=True.
