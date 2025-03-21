01b67490436415be08945bc0bbbfee16e53eb0d8
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-03-21 11:55:38 -0400

    TST Check that get_flags_str works for different int types

---
+++ ngmix/tests/test_flags.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import pytest
+from ..flags import get_flags_str, NAME_MAP, LOW_DET
+
+
+@pytest.mark.parametrize(
+    "dtype", [np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64, np.uint64]
+)
+def test_get_flags_str(dtype):
+    """Test that get_flags_str works for different integer types."""
+    val = np.array(LOW_DET, dtype=dtype)
+    flag_str = get_flags_str(val, NAME_MAP)
+    assert flag_str == NAME_MAP[LOW_DET]
