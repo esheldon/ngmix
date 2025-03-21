5394b5561d9d04c80458612f5bb7f53118baedfa
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-03-21 12:04:43 -0400

    STY Please flake8

--- ngmix/tests/test_flags.py
+++ ngmix/tests/test_flags.py
@@ -5,7 +5,8 @@ from ..flags import get_flags_str, NAME_MAP, LOW_DET
 
 
 @pytest.mark.parametrize(
-    "dtype", [np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64, np.uint64]
+    "dtype", [np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64,
+              np.uint64]
 )
 def test_get_flags_str(dtype):
     """Test that get_flags_str works for different integer types."""
