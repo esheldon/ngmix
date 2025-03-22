1de3f3bed25ae5bdd8012189c542e2ca67c20ff6
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-03-22 11:47:18 -0400

    [ci skip] DOC Comment on truncation with astype

--- ngmix/tests/test_flags.py
+++ ngmix/tests/test_flags.py
@@ -11,6 +11,9 @@ from ..flags import get_flags_str, NAME_MAP, LOW_DET
 def test_get_flags_str(dtype):
     """Test that get_flags_str works for different integer types."""
     # Set a high-enough bit in addition to the lower bit to ensure things work.
+    # This should be a flag that is defined in NAME_MAP.
+    # When converting to a type with fewer bits, only the least-significant
+    # bits are kept and the more-significant bits are discarded.
     val = np.array(LOW_DET | 2**45).astype(dtype)
     flag_str = get_flags_str(val, NAME_MAP)
     assert flag_str == NAME_MAP[LOW_DET]
