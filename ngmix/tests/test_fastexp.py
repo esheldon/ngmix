96399fb14c2102cedbd7a9f34342cef40c810b7a
Erin Sheldon <erin.sheldon@gmail.com>
2026-04-24 09:37:00 -0400

    better speed test for fastexp

--- ngmix/tests/test_fastexp.py
+++ ngmix/tests/test_fastexp.py
@@ -24,28 +24,21 @@ def _do_fexp(x):
     return csum
 
 
-def _do_exp(x):
-    csum = 0.0
-    for i in range(x.size):
-        csum += np.exp(x[i])
-
-    return csum
-
-
 def test_fastexp_timing():
-    x = np.array(vals)
+    x = np.linspace(-12, -7, 50)
+
     for _ in range(2):
         _do_fexp(x)
 
     t0 = time.time()
     for _ in range(1000):
-        _do_exp(x)
+        slow_sum = np.exp(x).sum()
     t0 = time.time() - t0
 
     t0f = time.time()
     for _ in range(1000):
-        _do_fexp(x)
+        fast_sum = _do_fexp(x)
     t0f = time.time() - t0f
 
-    # it should be faster
     assert t0f < t0, {'numpy': t0, 'fastexp': t0f}
+    assert np.allclose(slow_sum, fast_sum, rtol=4.0e-5)
